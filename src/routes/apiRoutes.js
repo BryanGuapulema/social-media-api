a6a87bcd968f5a8ade64fd5d0c555edbe2ec1c1c
BryanGuapulema <bryan19012003@gmail.com>
2025-09-04 23:31:09 -0500

    post routes and api cllent test

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -11,9 +11,12 @@ apiRouter.get('/', (req, res) => {
 apiRouter.post('/users', UserController.createUser)
 apiRouter.get('/users', UserController.getAllUsers)
 apiRouter.get('/users/:id', UserController.getUserById)
-apiRouter.put('/users/:id', UserController.updateUser)// todo
+apiRouter.put('/users/:id', UserController.updateUser)
 apiRouter.delete('/users/:id', UserController.deleteUser)
 
 // posts
 apiRouter.get('/posts', PostsController.getPosts)
 apiRouter.post('/posts', PostsController.createPosts)
+apiRouter.get('/posts/:id', PostsController.getPostById)
+apiRouter.put('/posts/:id', PostsController.updatePost)
+apiRouter.delete('/posts/:id', PostsController.deletePost)
